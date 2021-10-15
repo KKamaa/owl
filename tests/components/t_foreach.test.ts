cc15004a781b0ce1a8930eb6ce289e3345c292bf
Mathieu Duckerts-Antoine <dam@odoo.com>
2021-10-15 13:07:53 +0200

    [ADD] add a test for t-foreach directive

--- tests/components/t_foreach.test.ts
+++ tests/components/t_foreach.test.ts
@@ -1,4 +1,4 @@
-import { Component, mount, useState, xml } from "../../src/index";
+import { Component, mount, onMounted, useState, xml } from "../../src/index";
 import { makeTestFixture, nextTick, snapshotEverything, useLogLifecycle } from "../helpers";
 
 snapshotEverything();
@@ -196,5 +196,36 @@ describe("list of components", () => {
     parent.state.numbers = [1, 3];
     await nextTick();
     expect(fixture.innerHTML).toBe(`<div><p>1</p><p>3</p></div>`);
+  });
+
+  test("t-foreach with t-component, and update", async () => {
+    class Child extends Component {
+      static template = xml`
+          <span>
+            <t t-esc="state.val"/>
+            <t t-esc="props.val"/>
+          </span>`;
+      state = useState({ val: "A" });
+      setup() {
+        onMounted(() => {
+          this.state.val = "B";
+        });
+      }
+    }
+    class Parent extends Component {
+      static components = { Child };
+      static template = xml`
+          <div>
+            <t t-foreach="Array(2)" t-as="n" t-key="n_index">
+              <Child val="n_index"/>
+            </t>
+          </div>`;
+    }
+
+    await mount(Parent, fixture);
+    expect(fixture.innerHTML).toBe("<div><span>A0</span><span>A1</span></div>");
+
+    await nextTick(); // wait for changes triggered in mounted to be applied
+    expect(fixture.innerHTML).toBe("<div><span>B0</span><span>B1</span></div>");
   });
 });
